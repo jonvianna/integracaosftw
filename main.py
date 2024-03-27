b6205d1496092d4b32f79812e5a58378fbd7a11a
Jon Vianna <101150408+jonvianna@users.noreply.github.com>
2024-03-27 18:04:33 -0300

    Update main.py
    Adição do rota '/busca' para achar produtos por nome

--- main.py
+++ main.py
@@ -44,4 +44,35 @@ def create_produto():
     )
 
 
+@app.route('/busca', methods=['GET'])
+def get_produtos_por_nome():
+    nome_produto = request.args.get('nome')
+
+    if nome_produto is None:
+        return make_response(jsonify({"mensagem": "Por favor, forneça o parâmetro 'nome' na busca."}), 400)
+
+    db = Database()
+    produtos = db.consulta_produtos_por_nome(nome_produto)
+
+    if not produtos:
+        return make_response(jsonify({"mensagem": f"Nenhum produto encontrado com o nome '{nome_produto}'."}), 404)
+
+    produtos_formatados = []
+    for produto in produtos:
+        produto_formatado = {
+            'id': produto['id'],
+            'nome': produto['nome'],
+            'empresa': produto['empresa'],
+            'descricao': produto['descricao'],
+            'quantidade': produto['quantidade'],
+            'marca': produto['marca'],
+            'valor': produto['valor']
+        }
+        produtos_formatados.append(produto_formatado)
+
+    return make_response(jsonify(produtos_formatados))
+
+
+
+
 app.run(debug=True, host="0.0.0.0")
